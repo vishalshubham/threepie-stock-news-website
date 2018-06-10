de3865c20ca3332886173199559892e7727c5d9f
Vishal Chaudhary <vishal.shubham@gmail.com>
2018-06-10 18:44:08 -0400

    Added Axios
    - Added Axios package
    - Added Axios Data Requester file to call APIs
    
    Testing:
    http://localhost:3000/

---
+++ src/client/scripts/gateway/AxiosDataRequester.ts
@@ -0,0 +1,54 @@
+'use strict';
+
+import axios, { AxiosResponse } from 'axios';
+import { flatten } from 'lodash';
+import { KVMap } from 'src/client/scripts/data_models/general';
+
+export let baseURL;
+let withCredentials;
+
+// Sample URL: http://ec2-18-188-188-147.us-east-2.compute.amazonaws.com/api/v1/stockAndNews/getAll?symbol=AMZN
+const endpoints = {
+	beta: 'http://ec2-18-188-188-147.us-east-2.compute.amazonaws.com',
+	prod: 'http://ec2-18-188-188-147.us-east-2.compute.amazonaws.com'
+};
+
+// Note: you can either calling CREWS beta or your locally deployed CREWS
+if (process.env.NODE_ENV === 'development') {
+	baseURL = endpoints.beta;
+	withCredentials = true;
+}
+
+export default class AxiosDataRequester {
+	public static async get(url: string, params: KVMap<any>, timeout?: number, headers?: KVMap<any>) {
+ 		try {
+ 			const response: AxiosResponse = await axios.get(url, {
+ 				baseURL,
+ 				withCredentials,
+ 				timeout,
+ 				params,
+ 				headers,
+ 			});
+ 			return response.data;
+ 		} catch (error) {
+ 			console.error('API' + url + ' failed to get result');
+ 			throw error;
+ 		}
+ 	}
+
+ 	public static async post(url: string, data: KVMap<any>, timeout?: number, params?: KVMap<any>) {
+ 		try {
+ 			const response: AxiosResponse = await axios.post(url, data, {
+ 				baseURL,
+ 				withCredentials,
+ 				timeout,
+ 				params
+ 			});
+ 			return response.data;
+ 		} catch (error) {
+ 			console.error('API: ' + url + ' failed to get result');
+ 			throw error;
+ 		}
+ 	}
+}
+
